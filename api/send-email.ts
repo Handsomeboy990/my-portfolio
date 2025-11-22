a651a637695089a4408df8f4c39ff550301d00dd
Handsomeboy990 <lauretchacha@gmail.com>
2025-11-22 18:04:26 +0100

    fix: email

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,33 +1,43 @@
-import { Resend } from "resend";
+export const config = {
+    api: {
+      bodyParser: true,
+    },
+  };
+  
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+// console.log("env: ",     process.env.RESEND_API_KEY);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Méthode non autorisée' });
   }
 
   try {
-    const resend = new Resend(process.env.RESEND_API_KEY);
+    console.log("REQ BODY:", req.body);
 
     const { name, email, message } = req.body;
 
+    if (!name || !email || !message) {
+      return res.status(400).json({ error: 'Données manquantes' });
+    }
+
     const data = await resend.emails.send({
-      from: "Portfolio Contact <onboarding@resend.dev>",
-      to: "lauretchacha34@gmail.com",
-      subject: "Nouveau message depuis ton Portfolio",
-    //   reply_to: email,
+      from: `${name} <${email}>`, // DYNAMIQUE
+      to: ["contact@lauret.dev"], 
+      subject: "Nouveau message depuis le portfolio",
       html: `
         <h2>Nouveau message depuis ton portfolio</h2>
         <p><strong>Nom :</strong> ${name}</p>
         <p><strong>Email :</strong> ${email}</p>
-        <p><strong>Message :</strong></p>
-        <p>${message}</p>
+        <p><strong>Message :</strong><br>${message}</p>
       `,
     });
 
     return res.status(200).json({ success: true, data });
   } catch (error: any) {
-    console.error("Resend error:", error);
-    return res.status(500).json({ success: false, error: error.message });
+    return res.status(500).json({ error: error.message || 'Erreur serveur' });
   }
 }
