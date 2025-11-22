56f9a032e4344f64de38e3d5581497e878996fb8
Handsomeboy990 <lauretchacha@gmail.com>
2025-11-22 17:45:46 +0100

    fix: email

--- api/send-email.ts
+++ api/send-email.ts
@@ -4,7 +4,7 @@ export const config = {
     },
   };
   
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.VITE_RESEND_API_KEY);
